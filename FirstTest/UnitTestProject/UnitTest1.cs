113317ddeaf9c222f9408d8c5cfb46e724e246d9
pxt <conan1069@qq.com>
2020-09-22 09:48:22 +0800

    update

--- FirstTest/UnitTestProject/UnitTest1.cs
+++ FirstTest/UnitTestProject/UnitTest1.cs
@@ -4,11 +4,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
+            int a = 1, b = 2, result = 3;
+            sumtest s = new sumtest();
+            int sum =s.add(a, b);
+            Assert.AreEqual(sum, result); //Assert 断言
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            float a = 1.5f, b = 2.5f, result = 3.75f;
+            sumtest s = new sumtest();
+            float ji = s.cheng(a, b);
+            Assert.AreEqual(ji, result); //Assert 断言
+
         }
     }
 }
---
+++ FirstTest/UnitTestProject/sumtest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject
+{
+    class sumtest
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public float cheng(float a, float b)
+        {
+            return a * b;
+        }
+    }
+}
